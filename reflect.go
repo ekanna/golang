df0099190d5f2b36188187d492d3e2348d16de21
koti <ekanna.net@gmail.com>
2015-07-07 12:11:19 +0530

    Update reflect.go

--- reflect.go
+++ reflect.go
@@ -4,9 +4,29 @@ package main
 
 import (
   "reflect"
+  "log"
+  "os"
 )
 
+type Person struct {
+  Name string
+  Age int
+  IsMarried bool
+}
+
+var logger = log.New(os.Stdout, "mylog: ", log.Ldate+log.Ltime+log.Lshortfile)
+
 func main() {
-  
+  var p Person
+  GenricFunc(&p)  
+}
+
+func GenricFunc (structPtr interface{}) error {
+  // check whether structPtr is a pointer
+  value := reflect.ValueOf(structPtr)
+  if value.Kind() == reflect.Ptr {
+    logger.Println("%v is a pointer \n", structPtr)
+  }
+  return nil
 }
 
