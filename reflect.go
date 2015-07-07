bbc7ac0c7200c1b3c2d444c9c3b00c38cbb63537
koti <ekanna.net@gmail.com>
2015-07-07 14:13:51 +0530

    Update reflect.go

--- reflect.go
+++ reflect.go
@@ -35,3 +35,18 @@ func generic(strct interface{}){
 	value := reflect.New(val) // creates a pointervalue for the given type
 }
 
+// useful utility taken from https://github.com/jmoiron/sqlx package
+func baseType(t reflect.Type, expected reflect.Kind) (reflect.Type, error) {
+	t = deref(t)
+	if t.Kind() != expected {
+		return nil, fmt.Errorf("expected %s but got %s", expected, t.Kind())
+	}
+	return t, nil
+}
+
+func deref(t reflect.Type) reflect.Type {
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+	}
+	return t
+}
